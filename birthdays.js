41cfc9ada895c06aee6cb1fefbf69f00c65bf869
Ben Wheeler <wheeler.benjamin@gmail.com>
2024-08-15 17:14:08 -0400

    trying removing scope

--- birthdays.js
+++ birthdays.js
@@ -190,7 +190,7 @@ app.controller('AddressController', ['$scope', 'peopleData', function($scope, pe
       // console.log("spouse.last = " + spouse.last + "; trused = " + $sce.trustAsHtml(spouse.last));
       str = " and " + spouse.first + " " + spouse.last;
       if (!!spouse.birthday) {
-        str += " " + $scope.birthdayInlineStr(spouse, "MMM D");
+        str += " " + birthdayInlineStr(spouse, "MMM D");
       }
     }
     return str;
