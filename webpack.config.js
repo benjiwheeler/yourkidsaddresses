6731c9a33f39ba427bb2d2d474a75c992d01e721
Ben Wheeler <wheeler.benjamin@gmail.com>
2024-09-26 10:11:20 -0400

    changed dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/' // Note: may need to adjust this
   },
